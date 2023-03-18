79b3c1c20086f58deb2ac4dd52e50df76a1921f1
Max <maxmwangikenya@gmail.com>
2023-03-18 10:03:09 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(fun=`roller-skate`){
+    return `this Saturday, i want to ${fun}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork(`work from home`);
+
+function wrapAdjective(flair=`*`){
+    return function (adjective = `a hard worker`){
+        return `you are ${flair}${adjective}${flair}!`;
+    };
+}
+wrapAdjective(`||`)(`a dedicated programmer`);
